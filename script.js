49fa033a67f00cd672d901353a2012ad9f19b704
Upshur06 <mupshur2005@gmail.com>
2020-11-08 02:22:22 -0500

    declare variables in js

---
+++ script.js
@@ -0,0 +1,12 @@
+let boxs = document.querySelectorAll('.box');
+let scoreBoard = document.querySelector('.score-board');
+let timeKeeper = document.querySelector('.time');
+let startBtn = document.getElementById('start-button');
+let boxContainer = document.getElementById('box-container');
+
+let time = 60;
+let speed = 5000;
+let x1 = "";
+let points = "";
+let restart = true;
+
